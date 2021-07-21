38c0ecfcd4f5fb4739234922bb43a8c5120f86fb
NightPacks <87770166+NightPacks@users.noreply.github.com>
2021-07-21 17:30:22 +0200

    V1.0
    V1.0

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message, args) => {
 
     try {
         
-        var text = "**Lorange Admin** \n\n **__Commands__** \n !server - laat info over de minecraft server zien. \n !info - laat info over de discord server zien.";
+        var text = "**Lorange Admin** \n\n **__Commands__** \n ;server - laat info over de minecraft server zien. \n ;info - laat info over de discord server zien. \n ;help - geeft dit berichtje weer. \n **Staff** \n ;kick - kickt persoon van de server. \n ;ban - command om mensen te bannen. \n ;clear - cleart messages tot 50 berichten.";
 
         message.author.send(text);
 
